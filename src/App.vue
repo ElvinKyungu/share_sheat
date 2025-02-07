5ec23481e39225aa910bb868d5b070ae6c0f0a51
Elvin Code <85198945+ElvinKyungu@users.noreply.github.com>
2025-02-07 20:34:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="flex h-screen w-full justify-center bg-gray-100 items-center">
+    <h1>Hello everyone</h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
